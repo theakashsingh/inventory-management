4f747af5ea4a8c09ed2643c593e35c6b6948a710
Akash Singh <mr.singhaksh@gmail.com>
2023-07-21 18:09:39 +0530

    chore: update table sotries function

--- src/components/Table/Table.stories.ts
+++ src/components/Table/Table.stories.ts
@@ -1,5 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Table from './index';
+import DataItemType from '../../types/DataItemType';
+
+const generateTableRow = (
+    number: string,
+    mod3: string,
+    mod4: string,
+    mod5: string,
+    mod6: string
+) => ({ number, mod3, mod4, mod5, mod6 });
 
 const meta: Meta<typeof Table> = {
     component: Table,
@@ -9,189 +18,81 @@ const meta: Meta<typeof Table> = {
 export default meta;
 type Story = StoryObj<typeof Table>;
 
+// const columns = [
+//     {
+//         name: 'Number',
+//         selector: (row: any) => row['number'],
+//     },
+//     {
+//         name: 'Mod3',
+//         selector: (row: any) => row['mod3'],
+//     },
+//     {
+//         name: 'Mod4',
+//         selector: (row: any) => row['mod4'],
+//     },
+//     {
+//         name: 'Mod5',
+//         selector: (row: any) => row['mod5'],
+//     },
+//     {
+//         name: 'Mod6',
+//         selector: (row: any) => row['mod6'],
+//     },
+// ];
 
-const columns = [
-    {
-        name: "Number",
-        selector: (row:any) => row["number"],
-    },
-    {
-        name: "Mod3",
-        selector: (row:any) => row["mod3"],
-    },
-    {
-        name: "Mod4",
-        selector: (row:any) => row["mod4"],
-    },
-    {
-        name: "Mod5",
-        selector: (row:any) => row["mod5"],
-    },
-    {
-        name: "Mod6",
-        selector: (row:any) => row["mod6"],
-    }
-]
+// const columns = React.useMemo(() => {
+//     if (
+//         !(
+//             exportedData &&
+//             Array.isArray(exportedData) &&
+//             exportedData.length > 0
+//         )
+//     )
+//         return [];
+
+//     return Object.keys(exportedData[0]).map((key) => {
+//         return {
+//             name: key,
+//             selector: (row: DataItemType) => row[key],
+//         };
+//     });
+// }, [exportedData]);
+
+
+
 
 const data = [
-    {
-        number: '12',
-        mod3: '0',
-        mod4: '0',
-        mod5: '2',
-        mod6: '0',
-    },
-    {
-        number: '24',
-        mod3: '0',
-        mod4: '0',
-        mod5: '4',
-        mod6: '0',
-    },
-    {
-        number: '36',
-        mod3: '0',
-        mod4: '0',
-        mod5: '1',
-        mod6: '0',
-    },
-    {
-        number: '48',
-        mod3: '0',
-        mod4: '0',
-        mod5: '3',
-        mod6: '0',
-    },
-    {
-        number: '60',
-        mod3: '0',
-        mod4: '0',
-        mod5: '0',
-        mod6: '0',
-    },
-    {
-        number: '72',
-        mod3: '0',
-        mod4: '0',
-        mod5: '2',
-        mod6: '0',
-    },
-    {
-        number: '84',
-        mod3: '0',
-        mod4: '0',
-        mod5: '4',
-        mod6: '0',
-    },
-    {
-        number: '96',
-        mod3: '0',
-        mod4: '0',
-        mod5: '1',
-        mod6: '0',
-    },
-    {
-        number: '108',
-        mod3: '0',
-        mod4: '0',
-        mod5: '3',
-        mod6: '0',
-    },
-    {
-        number: '120',
-        mod3: '0',
-        mod4: '0',
-        mod5: '0',
-        mod6: '0',
-    },
-    {
-        number: '132',
-        mod3: '0',
-        mod4: '0',
-        mod5: '2',
-        mod6: '0',
-    },
-    {
-        number: '144',
-        mod3: '0',
-        mod4: '0',
-        mod5: '4',
-        mod6: '0',
-    },
-    {
-        number: '156',
-        mod3: '0',
-        mod4: '0',
-        mod5: '1',
-        mod6: '0',
-    },
-    {
-        number: '168',
-        mod3: '0',
-        mod4: '0',
-        mod5: '3',
-        mod6: '0',
-    },
-    {
-        number: '180',
-        mod3: '0',
-        mod4: '0',
-        mod5: '0',
-        mod6: '0',
-    },
-    {
-        number: '192',
-        mod3: '0',
-        mod4: '0',
-        mod5: '2',
-        mod6: '0',
-    },
-    {
-        number: '204',
-        mod3: '0',
-        mod4: '0',
-        mod5: '4',
-        mod6: '0',
-    },
-    {
-        number: '216',
-        mod3: '0',
-        mod4: '0',
-        mod5: '1',
-        mod6: '0',
-    },
-    {
-        number: '228',
-        mod3: '0',
-        mod4: '0',
-        mod5: '3',
-        mod6: '0',
-    },
-    {
-        number: '240',
-        mod3: '0',
-        mod4: '0',
-        mod5: '0',
-        mod6: '0',
-    },
-    {
-        number: '252',
-        mod3: '0',
-        mod4: '0',
-        mod5: '2',
-        mod6: '0',
-    },
-    {
-        number: '264',
-        mod3: '0',
-        mod4: '0',
-        mod5: '4',
-        mod6: '0',
-    },
-]
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+    generateTableRow('12', '0', '0', '2', '0'),
+];
+
+console.log(data);
+
+
+const generateColumns = () =>{
+    return Object.keys(data[0]).map((key)=>{
+       return {
+           name: key,
+           selector: (row: DataItemType) => row[key],
+       };
+    })
+    
+}
+
+const columns = generateColumns()
 export const Base: Story = {
     args: {
         columns:columns,
-        data: data
+        data: data,
     },
 };
